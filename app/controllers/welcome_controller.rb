626aacd6909e53eea4497cde6a9fb0683dd03f1c
Samuel Rund <Sam@Rundtime.com>
2015-05-05 10:28:46 -0400

    Add a contoller action

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,10 @@
 class WelcomeController < ApplicationController
+	def index
+		@daves_favoriteLanaguge = sample_languages()
+	end
+
+	def sample_languages
+			['ruby', 'swift', 'javascript', 'c#'].sample
+	end
+	
 end
